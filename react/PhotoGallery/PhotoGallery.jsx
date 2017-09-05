1b5f7a0130aba745919ebbe687dbc8c952170914
James Burke <jjaburke@hotmail.com>
2017-09-05 17:13:29 +0100

    Styled gallery.

--- react/PhotoGallery/PhotoGallery.jsx
+++ react/PhotoGallery/PhotoGallery.jsx
@@ -25,18 +25,18 @@ class PhotoGallery extends React.Component {
 
                 <div className="gallery__img-container noselect">
                     <img className="gallery__img" src={currentPhoto.imageUrl} />
-                    <h2 className="gallery__title">{currentPhoto.title}</h2>
+                    <h2 className="gallery__title primary-font header-fuzzy-text header-fuzzy-text--dark">{currentPhoto.title}</h2>
                 </div>
 
                 <div id="next-photo-icon" onClick={this.props.nextPhoto} className={'noselect ' + (isLast ? 'hidden' : '')}>
-                    <i className="fa fa-caret-right" aria-hidden="true"> </i>
+                    <i className="fa fa-caret-right header-fuzzy-text header-fuzzy-text--dark" aria-hidden="true"> </i>
                 </div>
                 <div id="prev-photo-icon" onClick={this.props.previousPhoto} className={'noselect ' + (isFirst ? 'hidden' : '')}>
-                    <i className="fa fa-caret-left" aria-hidden="true"> </i>
+                    <i className="fa fa-caret-left header-fuzzy-text header-fuzzy-text--dark" aria-hidden="true"> </i>
                 </div>
 
                 <div id="close-gallery-icon" onClick={this.props.closeGallery} className="noselect">
-                    <i className="fa fa-times" aria-hidden="true"> </i>
+                    <i className="fa fa-times header-fuzzy-text header-fuzzy-text--dark" aria-hidden="true"> </i>
                 </div>
             </div>
         );
